315999a4f8be238b69ae84f9dfc18a9b8e696d11
Sorin <sorin.biriescu@gmail.com>
2017-09-13 23:14:30 +0200

    split index.html to base.html and index.html for jinja2 templates

--- serenity4/serenity4.py
+++ serenity4/serenity4.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+@app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello World!'
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
