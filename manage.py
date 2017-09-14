579119e56e25f68b71b787cd86853a0fc7dc6c77
Sorin <sorin.biriescu@gmail.com>
2017-09-14 12:07:09 +0200

    * added __init__, models, forms, views and manage.py * commented out serenity4.py

---
+++ manage.py
@@ -0,0 +1,20 @@
+from serenity4 import app, db
+from serenity4.models import Jobs, User, UserJobStatus
+from flask_script import Manager, prompt_bool
+from flask_migrate import Migrate, MigrateCommand
+
+@manager.command
+def initdb():
+    db.create_all()
+    db.session.commit()
+    print 'Initialized the database'
+
+@manager.command
+def dropdb():
+    if prompt_bool(
+        "Are you sure you want to lose all your data"):
+        db.drop_all()
+        print 'Dropped the database'
+
+if __name__ == '__main__':
+    manager.run()
